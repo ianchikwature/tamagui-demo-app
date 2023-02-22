9ba3a436d72ab43ee6c5028f6c1ca008723bedb6
Ian Chikwature <ianchikwature@hotmail.co.uk>
2023-02-22 07:50:18 +0000

    created repo

---
+++ apps/expo/App.tsx
@@ -0,0 +1,25 @@
+import 'expo-dev-client'
+import React from 'react'
+import { NativeNavigation } from 'app/navigation/native'
+import { Provider } from 'app/provider'
+import { useFonts } from 'expo-font'
+
+export default function App() {
+
+  console.log("native env: ", process.env.AUTH_API)
+
+  const [loaded] = useFonts({
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+  })
+
+  if (!loaded) {
+    return null
+  }
+
+  return (
+    <Provider>
+      <NativeNavigation />
+    </Provider>
+  )
+}
---
+++ apps/next/pages/_app.tsx
@@ -0,0 +1,48 @@
+import '@tamagui/core/reset.css'
+import '@tamagui/font-inter/css/400.css'
+import '@tamagui/font-inter/css/700.css'
+import 'raf/polyfill'
+
+import { NextThemeProvider, useRootTheme } from '@tamagui/next-theme'
+import { Provider } from 'app/provider'
+import Head from 'next/head'
+import React, { startTransition } from 'react'
+import type { SolitoAppProps } from 'solito'
+
+function MyApp({ Component, pageProps }: SolitoAppProps) {
+ 
+ console.log("Web env test: ", process.env.AUTH_API) 
+
+ return (
+    <>
+      <Head>
+        <title>Tamagui Example App</title>
+        <meta name="description" content="Tamagui, Solito, Expo & Next.js" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
+
+function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [theme, setTheme] = useRootTheme()
+
+  return (
+    <NextThemeProvider
+      onChangeTheme={(next) => {
+        startTransition(() => {
+          setTheme(next)
+        })
+      }}
+    >
+      <Provider disableRootThemeClass defaultTheme={theme}>
+        {children}
+      </Provider>
+    </NextThemeProvider>
+  )
+}
+
+export default MyApp
